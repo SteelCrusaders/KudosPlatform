f1a4a6db3c4bc64084c1b2a06105bb23b0587ff9
Daniel Scott-Raynsford <plagueho@gmail.com>
2016-12-23 20:57:58 +1300

    First attempt at Azure install task

--- README.md
+++ README.md
@@ -4,7 +4,89 @@
 [![Join the chat at https://gitter.im/KudosPlatform/Lobby](https://badges.gitter.im/KudosPlatform/Lobby.svg)](https://gitter.im/KudosPlatform/Lobby?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge)
 [![Build status](https://ci.appveyor.com/api/projects/status/n234885s6cgmeb45/branch/master?svg=true)](https://ci.appveyor.com/project/IAG-NZ/kudosplatform/branch/master)
 
-The IAG-NZ Dev Guild KudosPlatform for gamifying Yammer
+The IAG-NZ Dev Guild KudosPlatform for gamifying Yammer.
+
+## Requirements
+
+To work with this project you will need to have installed:
+
+1. Windows Management Framework 5.0
+   - Windows 10/Windows Server 2016 - already installed.
+   - Earlier Windows versions - [from this location](https://www.microsoft.com/en-us/download/details.aspx?id=50395).
+2. Visual Studio 2015 (Community, Pro or Enterpirse)
+   - Community can be downloaded and installed free [from this location](https://www.visualstudio.com/vs/community/).
+3. An Azure account with a valid subscription
+   - If you have an MSDN account then you will be able to use this to get a free Azure subscription with a free monthly credit allowance.
+4. Your Azure subscription must contain an Azure Active Directory
+   - It will have a default one if you haven't deleted it.
+   - This is required to create a service principal for the PowerShell automation to use to install this project.
+   - See [this page](https://docs.microsoft.com/en-us/azure/azure-resource-manager/resource-group-authenticate-service-principal) for more information.
+
+## PSake
+
+This project includes a [PSake](https://github.com/psake/psake) file that defines tasks for initializing the development/build environment, building, installing and deploying the project.
+
+To use the PSake file (default.ps1), you will need to install PSake.
+
+> To install PSake:
+
+1. At an Administrator Windows PowerShell console enter:
+
+```PowerShell
+Install-Module -Name PSake
+```
+
+### Init
+
+The _Init_ task will initialize the build or development environment:
+
+```PowerShell
+Invoke-PSake Init
+```
+
+> Note: This task could take over 10 minutes to complete, but only needs to be performed once on a node.
+
+### Clean
+
+The _Clean_ task will clean the build environment:
+
+```PowerShell
+Invoke-PSake Clean
+```
+
+### Build
+
+The _Build_ task will build the project:
+
+```PowerShell
+Invoke-PSake Build
+```
+
+### Install
+
+The _Install_ task will build the project and then install it to Azure.
+Azure credentials will need to be provided:
+
+```PowerShell
+Invoke-PSake Install -parameters @{ azureUSername = '03becdd7-3395-4bf2-8eff-528b7dcc9a07'; azurePassword ='P@ssword!1'; azureTenantId = 'fef06518-9f81-4cff-946a-083d33dd17db' }
+```
+
+### CreateAzureSP
+
+The _CreateAzureSP_ task will create the service principal in Azure AD that will be used as the account to login to Azure to install this project.
+This only needs to be done once for any Azure account that will have the KudosPlatform installed to.
+
+```PowerShell
+Invoke-PSake CreateAzureSP -parameters @{ azureADDomain = 'MyAzureAppDomain'; azurePassword = 'P@ssword!1' }
+```
+
+### GetAzureSP
+
+The _GetAzureSP_ task will return service principal name in Azure AD that should be used as the username for an account to login to Azure to install this project.
+
+```PowerShell
+Invoke-PSake GetAzureSP
+```
 
 
 ## Versions
--- default.ps1
+++ default.ps1
@@ -1,4 +1,7 @@
 properties {
+  # General properties
+  $projectName = 'KudosPlatform'
+
   # Get the properties/parameters for deploying to Azure
   if ([String]::IsNullOrEmpty($azureUsername)) {
     if (-not [String]::IsNullOrEmpty($ENV:azureUsername)) {
@@ -13,6 +16,13 @@ properties {
         -AsPlainText `
         -Force
     } # if
+  } else {
+    if ($azurePassword -is [String]) {
+        $azurePassword = ConvertTo-SecureString `
+          -String $azurePassword `
+          -AsPlainText `
+          -Force
+    } # if
   } # if
 
   if (-not [String]::IsNullOrEmpty($azureUsername) -and `
@@ -22,25 +32,36 @@ properties {
       -ArgumentList @($azureUsername, $azurePassword)
   } # if
 
+  if ([String]::IsNullOrEmpty($azureTenantId)) {
+    if (-not [String]::IsNullOrEmpty($ENV:azureTenantId)) {
+      $azureTenantId = $ENV:azureTenantId
+    } # if
+  } # if
+
+  # Task messages
   $initMessage = 'Executed Init!'
   $cleanMessage = 'Executed Clean!'
   $testMessage = 'Executed Test!'
   $buildMessage = 'Executed Build!'
-  $deployMessage = 'Executed Deploy!'
+  $installMessage = 'Executed Install!'
 }
 
 task default -depends Test
 
-task Deploy -depends Init, Build, Clean, Test {
+task Install -depends Init, Build, Clean, Test {
   if (-not $azureCredential) {
-    Write-Error -Message 'Deployment failed because Azure credentials not provided.'
+    Write-Error -Message 'Installation failed because Azure credentials not provided.'
     return
   }
 
-  Login-AzureRmAccount `
-    -Credential $azureCredential
+  Add-AzureRmAccount `
+    -Credential $azureCredential `
+    -TenantId $azureTenantId `
+    -ServicePrincipal
 
-  $deployMessage
+  Write-SuccessfulTaskInfo -Message 'Successfully logged into Azure Account.'
+
+  $installMessage
 }
 
 task Test -depends Init, Build, Clean {
@@ -59,26 +80,91 @@ task Init {
   # Install Microsoft Azure App Service Tools v2.9.6 - Visual Studio 2015
   if (Get-Package -ProviderName msi |
     Where-Object -Filter { $_.TagId -eq '4EB8A716-D680-3AF9-B117-BD568DD43B0D' }) {
-    Write-Host 'Microsoft Azure App Service Tools v2.9.6 - Visual Studio 2015 is already installed' -foregroundcolor Green
+    Write-SuccessfulTaskInfo -Message 'Microsoft Azure App Service Tools v2.9.6 - Visual Studio 2015 is already installed'
   } else {
-    Write-Host 'Installing Microsoft Azure App Service Tools v2.9.6 - Visual Studio 2015...' -foregroundcolor Yellow
+    Write-ProgressTaskInfo -Message 'Installing Microsoft Azure App Service Tools v2.9.6 - Visual Studio 2015...'
     $WebToolsAzurePath = Join-Path -Path $ENV:Temp -ChildPath 'WebToolsAzure2015.exe'
     Invoke-WebRequest -Uri https://aka.ms/azfunctiontools -OutFile $WebToolsAzurePath
     & $WebToolsAzurePath @('/install','/quiet','/norestart') | Out-Null
     Remove-Item -Path $WebToolsAzurePath -Force
   } # if
 
-  # Install Azure PowerShell Module so that we can Deploy components to Azure
+  # Install Azure PowerShell Module so that we can Install components to Azure
   if (Get-Module -Name Azure -ListAvailable) {
-    Write-Host 'Microsoft Azure PowerShell Module is already installed' -foregroundcolor Green
+    Write-SuccessfulTaskInfo -Message 'Microsoft Azure PowerShell Module is already installed'
   } else {
-    Write-Host 'Installing Microsoft Azure PowerShell Module...' -foregroundcolor Yellow
+    Write-ProgressTaskInfo -Message 'Installing Microsoft Azure PowerShell Module...'
     Install-Module -Name Azure -Force -AllowClobber
+  } # if
+
+  # Install AzureRM PowerShell Module so that we can Install components to Azure
+  if (Get-Module -Name AzureRM -ListAvailable) {
+    Write-SuccessfulTaskInfo -Message 'Microsoft AzureRM PowerShell Module is already installed'
+  } else {
+    Write-ProgressTaskInfo -Message 'Installing Microsoft AzureRM PowerShell Module...'
+    Install-Module -Name AzureRM -Force -AllowClobber
   } # if
 
   $initMessage
 }
 
+task CreateAzureSP {
+  <#
+    This task creates an Azure Service Principal in Azure AD that will be used for all installation automation.
+    This can only be run interactively as the Login-AzureRmAccount will pop up an interactive window for the
+    user to log in with.
+  #>
+  $account = Login-AzureRmAccount
+  Write-Host -Object "Creating Application in Azure AD" -foregroundcolor Yellow
+  $app = New-AzureRmADApplication `
+    -DisplayName $projectName `
+    -HomePage "https://$azureADDomain/$projectName" `
+    -IdentifierUris "https://$azureADDomain/$projectName" `
+    -Password $azurePassword
+  Write-ProgressTaskInfo -Message "Creating Azure AD Service Principal for ApplicationId '$($app.ApplicationId)'"
+  $null = New-AzureRmADServicePrincipal `
+    -ApplicationId $app.ApplicationId
+  Write-ProgressTaskInfo -Message "Assigning role Contributor to AD Service Principal for ApplicationId '$($app.ApplicationId)'"
+  $roleAssignment = $null
+  while (-not $roleAssignment) {
+    $roleAssignment = New-AzureRmRoleAssignment `
+      -RoleDefinitionName Contributor `
+      -ServicePrincipalName $app.ApplicationId `
+      -ErrorAction SilentlyContinue
+  } # while
+  Write-SuccessfulTaskInfo -Message "'$projectName' is registered with the Application Id '$($app.ApplicationId)'."
+  Write-SuccessfulTaskInfo -Message "This should be used as the username when logging into Azure to deploy this project."
+  Write-SuccessfulTaskInfo -Message "Use a Tenant Id of '$($account.Context.Tenant.TenantId)'."
+}
+
+task GetAzureSP {
+  <#
+    This task retrieves the existing Azure AD Service Principal for this application.
+  #>
+  $account = Login-AzureRmAccount
+  Write-Host -Object "Getting Application from Azure AD" -foregroundcolor Yellow
+  $app = Get-AzureRmADApplication -DisplayNameStartWith $projectName
+  Write-SuccessfulTaskInfo -Message "'$projectName' is registered with the Application Id '$($app.ApplicationId)'."
+  Write-SuccessfulTaskInfo -Message "This should be used as the username when logging into Azure to deploy this project."
+  Write-SuccessfulTaskInfo -Message "Use a Tenant Id of '$($account.Context.Tenant.TenantId)'."
+}
+
 task ? -Description "Helper to display task info" {
   Write-Documentation
+}
+
+function Write-SuccessfulTaskInfo {
+  [CmdLetBinding()]
+  Param (
+    [String] $Message
+  )
+  Write-Host -Object $Message -foregroundcolor Green
+}
+
+function Write-ProgressTaskInfo {
+  [CmdLetBinding()]
+  Param (
+    [String] $Message
+  )
+  Write-Host -Object $Message -foregroundcolor White
 }
